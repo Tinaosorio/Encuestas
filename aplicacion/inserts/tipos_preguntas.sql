c8b66cde4fa0855be1f6cbf80c5d27685eb352d1
Valentina Osorio <valen.6554@hotmail.com>
2019-06-27 23:46:54 -0500

    Se agrega insert de tipos de rp

---
+++ aplicacion/inserts/tipos_preguntas.sql
@@ -0,0 +1,6 @@
+insert  into `aplicacion_tipopregunta`(`id`,`nombre`,`descripcion`) values 
+	(1,'Activo-Reflexivo','Activo-Reflexivo'),
+	(2,'Sensorial-Intuitivo','Sensorial-Intuitivo'),
+	(3,'Visual-Verbal','Visual-Verbal'),
+	(4,'Secuencial-Global','Secuencial-Global');
+
